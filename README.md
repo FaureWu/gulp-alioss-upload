93cd0f47cce32c2e4fafa6378ce085dff733480f
yaochen <yaochen@guoxiaomei.com>
2018-11-14 10:36:08 +0800

    version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# gulp-alioss-upload
-a gulp plugin to upload file to alioss
+# ALI OSS UPLOAD
+
+用于上传到阿里云oss服务器的gulp插件
+
+## 安装
+
+```bash
+$ npm install --dev gulp-alioss-upload
+```
+or
+```bash
+$ yarn add -dev gulp-alioss-upload
+```
+
+## 使用方法
+
+在gulp配置文件gulpfile.js中新增task任务
+
+```js
+const gulp = require('gulp')
+const oss = require('gulp-alioss-upload')
+
+gulp.task('upload:oss', function() {
+  const options = {
+    accessKeyId: "**************",
+    accessKeySecret: "*************",
+    endpoint: "https://***********.aliyuncs.com",
+    region: "***********",
+    bucket: "*********",
+    path: "dist/assets/",
+    formats: ['png', 'jpeg', 'jpg', 'svg'],
+  }
+  return gulp.src(['src/**/*.js', 'src/**/*.css'])
+    .pipe(oss(options))
+    .pipe(gulp.dest('./dist/'))
+})
+```
+
+## 参数介绍
+* accessKeyId 必填 阿里云oss key
+* accessKeySecret 必填 阿里云oss secret
+* endpoint 必填 阿里云oss服务器地址
+* region 必填 阿里云oss region
+* bucket 必填 阿里云oss bucket
+* path 选填 默认值/ 需要上传的资源根路径
+* formats 选填 默认值['png', 'jpg', 'jpeg', 'svg', 'bmp', 'gif', 'webp', 'tiff'] 可上传的资源格式
---
+++ index.js
@@ -0,0 +1,137 @@
+const through = require("through2");
+const ALIOSS = require("ali-oss");
+const fs = require("fs");
+const pathUtil = require("path");
+const md5File = require("md5-file");
+const process = require("process");
+const color = require("ansi-colors");
+
+/**
+ *
+ * @param {Object} options
+ * @param {String} options.accessKeyId
+ * @param {String} options.accessKeySecret
+ * @param {String} options.endpoint
+ * @param {String} options.region
+ * @param {String} options.bucket
+ * @param {String} options.path
+ * @param {Array} options.formats 可选，默认['png', 'jpg', 'jpeg', 'svg', 'bmp', 'gif', 'webp', 'tiff']
+ * @param {String} options.prefix 可选，默认为@oss
+ */
+module.exports = function(options) {
+  const {
+    accessKeyId,
+    accessKeySecret,
+    endpoint,
+    region,
+    bucket,
+    path = '/',
+    formats = ['png', 'jpg', 'jpeg', 'svg', 'bmp', 'gif', 'webp', 'tiff'],
+    prefix = '@oss'
+  } = options;
+  const ossClient = new ALIOSS({
+    accessKeyId,
+    accessKeySecret,
+    endpoint,
+    region,
+    bucket
+  });
+
+  const uploadCache = {};
+  let count = 0;
+
+  return through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      this.push(file);
+      return cb();
+    }
+
+    if (file.isStream()) {
+      this.emit(
+        "error",
+        new gutil.PluginError(PLUGIN_NAME, "Streaming not supported")
+      );
+      return cb();
+    }
+
+    const formatStr = formats.reduce((result, format) => {
+      if (result) return `${result}|.${format}`
+
+      return `.${format}`
+    }, '')
+    const reg = new RegExp(`@oss/\\S+(${formatStr})`, 'gi')
+    let content = file.contents.toString();
+
+    const matches = content.match(reg);
+    const checkTask = () => {
+      if (matches.length <= 0 && count <= 0) {
+        file.contents = new Buffer(content);
+        this.push(file);
+        cb();
+      }
+    };
+    if (matches) {
+      do {
+        const match = matches.pop();
+        const realPath = pathUtil.resolve(path, match.replace(`${prefix}/`, ''))
+        const showPath = pathUtil.join(path, match.replace(`${prefix}/`, ''))
+        if (!fs.existsSync(realPath)) {
+          console.log(
+            color.red("上传CND失败"),
+            " ",
+            "图片资源",
+            " ",
+            showPath,
+            " ",
+            "文件不存在"
+          );
+          checkTask();
+          continue;
+        }
+
+        const fileKey = md5File.sync(realPath);
+        if (uploadCache[fileKey]) {
+          checkTask();
+          continue;
+        }
+
+        uploadCache[fileKey] = true;
+        count += 1;
+        ossClient
+          .put(fileKey, realPath)
+          .then(result => {
+            console.log(
+              color.yellow("上传CDN成功"),
+              " ",
+              "图片资源",
+              " ",
+              showPath,
+              " ",
+              result.url
+            );
+            content = content.replace(new RegExp(match, "g"), result.url);
+            count -= 1;
+            checkTask();
+          })
+          .catch(error => {
+            console.log(
+              color.red("上传CND失败"),
+              " ",
+              "图片资源",
+              " ",
+              showPath,
+              " ",
+              error.message
+            );
+            uploadCache[fileKey] = false;
+            count -= 1;
+            checkTask();
+          });
+      } while (matches.length > 0);
+    } else {
+      file.contents = new Buffer(content);
+      this.push(file);
+      cb();
+    }
+  });
+};
